6941b5c886b58e5c781042e76208c405b114d5ae
Juha Reunanen <juha.reunanen@tomaattinen.com>
2018-06-28 15:31:22 +0300

    Improve find_optimal_threshold

--- dlib/dnn/find_optimal_threshold.h
+++ dlib/dnn/find_optimal_threshold.h
@@ -9,6 +9,9 @@
 #include "../image_processing/box_overlap_testing.h"
 #include "../image_processing/full_object_detection.h"
 
+#include <unordered_set>
+#include <unordered_map>
+
 namespace dlib
 {
 
@@ -40,8 +43,9 @@ namespace dlib
     double find_optimal_threshold(
         const std::vector<std::vector<mmod_rect>>& truth,
         const std::vector<std::vector<mmod_rect>>& detections,
-        double truth_match_iou_threshold_for_correct_label = 0.1,
-        double truth_match_iou_threshold_for_incorrect_label = 0.5
+        double truth_match_iou_threshold_for_correct_label = 0.25,
+        double truth_match_iou_threshold_for_incorrect_label = 0.5,
+        double truth_match_iou_threshold_for_ignore = 0.25
     )
     {
         DLIB_CASSERT(!detections.empty());
@@ -52,60 +56,106 @@ namespace dlib
         double minimum_detection_confidence = std::numeric_limits<double>::max();
         double maximum_detection_confidence = -std::numeric_limits<double>::max();
 
-        for (size_t i = 0, sample_count = truth.size(); i < sample_count; ++i)
+        const int sample_count = static_cast<int>(truth.size());
+
+#pragma omp parallel for
+        for (int i = 0; i < sample_count; ++i)
         {
             const std::vector<mmod_rect>& truth_i = truth[i];
             const std::vector<mmod_rect>& detections_i = detections[i];
 
-            const int detections_i_size = static_cast<int>(detections_i.size());
+            const size_t detections_i_size = detections_i.size();
+            const size_t truth_i_size = truth_i.size();
 
-#pragma omp parallel for
-            for (int j = 0; j < detections_i_size; ++j)
+            //                 truth index       iou     detection index
+            std::unordered_map<size_t, std::pair<double, size_t>> best_matching_truths;
+            std::unordered_set<size_t> detection_indexes_that_can_be_ignored;
+
+            for (size_t j = 0; j < detections_i_size; ++j)
             {
                 const mmod_rect& detection = detections_i[j];
 
-                bool found_corresponding_truth = false;
                 bool found_corresponding_ignore = false;
-                for (const mmod_rect& candidate_truth : truth_i)
+
+                for (size_t k = 0; k < truth_i_size; ++k)
                 {
+                    const mmod_rect& candidate_truth = truth_i[k];
                     const double truth_match_iou = box_intersection_over_union(detection.rect, candidate_truth.rect);
+
                     const auto accept_with_correct_label = [&]()
                     {
                         return truth_match_iou >= truth_match_iou_threshold_for_correct_label && detection.label == candidate_truth.label;
                     };
+
+                    const auto accept_ignore = [&]()
+                    {
+                        return candidate_truth.ignore && truth_match_iou >= truth_match_iou_threshold_for_ignore;
+                    };
+
                     if (accept_with_correct_label() || truth_match_iou >= truth_match_iou_threshold_for_incorrect_label)
                     {
-                        if (candidate_truth.ignore)
+                        const auto truth_match_iou_and_detection_index = [&]() { return std::make_pair(truth_match_iou, j); };
+                        const auto i = best_matching_truths.find(k);
+                        if (i == best_matching_truths.end())
                         {
-                            found_corresponding_ignore = true;
+                            best_matching_truths[k] = truth_match_iou_and_detection_index();
                         }
-                        else
+                        else if (truth_match_iou > i->second.first)
                         {
-                            found_corresponding_truth = true;
-                            break;
+                            i->second = truth_match_iou_and_detection_index();
                         }
                     }
+
+                    if (accept_ignore())
+                    {
+                        found_corresponding_ignore = true;
+                    }
                 }
+
+                if (found_corresponding_ignore)
+                {
+                    detection_indexes_that_can_be_ignored.insert(j);
+                }
+            }
+
+            std::unordered_set<size_t> detection_indexes_that_have_corresponding_best_match_truth;
+
+            for (const auto& j : best_matching_truths)
+            {
+                detection_indexes_that_have_corresponding_best_match_truth.insert(j.second.second);
+            }
 
 #pragma omp critical
-                {
-                    if (found_corresponding_truth)
-                    {
-                        true_detections.push_back(detection.detection_confidence);
-                    }
-                    else if (!found_corresponding_ignore)
-                    {
-                        false_detections.push_back(detection.detection_confidence);
-                    }
+            for (size_t j = 0; j < detections_i_size; ++j)
+            {
+                const mmod_rect& detection = detections_i[j];
 
-                    minimum_detection_confidence = std::min(minimum_detection_confidence, detection.detection_confidence);
-                    maximum_detection_confidence = std::max(maximum_detection_confidence, detection.detection_confidence);
+                const auto best_matching_truth_found = [&]()
+                {
+                    return detection_indexes_that_have_corresponding_best_match_truth.find(j) != detection_indexes_that_have_corresponding_best_match_truth.end();
+                };
+
+                const auto can_be_ignored = [&]()
+                {
+                    return detection_indexes_that_can_be_ignored.find(j) != detection_indexes_that_can_be_ignored.end();
+                };
+
+                if (best_matching_truth_found())
+                {
+                    true_detections.push_back(detection.detection_confidence);
                 }
+                else if (!can_be_ignored())
+                {
+                    false_detections.push_back(detection.detection_confidence);
+                }
+
+                minimum_detection_confidence = std::min(minimum_detection_confidence, detection.detection_confidence);
+                maximum_detection_confidence = std::max(maximum_detection_confidence, detection.detection_confidence);
             }
         }
 
         if (true_detections.empty() && false_detections.empty())
-        {            
+        {
             return 0.0; // TODO: what should we do here?
         }
 
