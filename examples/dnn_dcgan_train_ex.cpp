4fcfe66e514c79693a36e9e88da7ce5a916085e8
Juha Reunanen <juha.reunanen@tomaattinen.com>
2020-03-30 16:37:42 +0300

    Support RGB images having size 512 x 512

--- examples/dnn_dcgan_train_ex.cpp
+++ examples/dnn_dcgan_train_ex.cpp
@@ -18,10 +18,6 @@
     Each training iteration alternates between training the discriminator and the generator.
     We first train the discriminator with real and fake images and then use the gradient from
     the discriminator to update the generator.
-
-    In this example, we are going to learn how to generate digits from the MNIST dataset, but
-    the same code can be run using the Fashion MNIST datset:
-    https://github.com/zalandoresearch/fashion-mnist
 */
 
 #include <algorithm>
@@ -55,7 +51,7 @@ public:
 };
 
 // Some helper definitions for the noise generation
-const size_t noise_size = 100;
+const size_t noise_size = 1000;
 using noise_t = std::array<matrix<float, 1, 1>, noise_size>;
 
 noise_t make_noise(dlib::rand& rnd)
@@ -81,42 +77,62 @@ using contp = add_layer<cont_<num_filters, kernel_size, kernel_size, stride, str
 // training, we just stack a compatible one on top to be able to have a () operator on the
 // generator.
 using generator_type =
-    loss_binary_log_per_pixel<
-    sig<contp<1, 4, 2, 1,
-    relu<bn_con<contp<64, 4, 2, 1,
-    relu<bn_con<contp<128, 3, 2, 1,
-    relu<bn_con<contp<256, 4, 1, 0,
+    loss_multiclass_log_per_pixel<
+    sig<contp<3, 4, 2, 0,
+    relu<bn_con<contp<64, 3, 2, 0,
+    relu<bn_con<contp<64, 3, 2, 0,
+    relu<bn_con<contp<128, 3, 2, 0,
+    relu<bn_con<contp<128, 3, 2, 0,
+    relu<bn_con<contp<256, 3, 2, 0,
+    relu<bn_con<contp<512, 3, 2, 1,
+    relu<bn_con<contp<1024, 4, 1, 0,
     input<noise_t>
-    >>>>>>>>>>>>;
+    >>>>>>>>>>>>>>>>>>>>>>>>;
 
 // Now, let's proceed to define the discriminator, whose role will be to decide whether an
 // image is fake or not.
 using discriminator_type =
     loss_binary_log<
     conp<1, 3, 1, 0,
+    leaky_relu<bn_con<conp<1024, 4, 2, 0,
+    leaky_relu<bn_con<conp<512, 4, 2, 1,
     leaky_relu<bn_con<conp<256, 4, 2, 1,
     leaky_relu<bn_con<conp<128, 4, 2, 1,
+    leaky_relu<bn_con<conp<128, 4, 2, 1,
+    leaky_relu<bn_con<conp<64, 4, 2, 1,
     leaky_relu<conp<64, 4, 2, 1,
-    input<matrix<unsigned char>>
-    >>>>>>>>>>;
+    input<matrix<rgb_pixel>>
+    >>>>>>>>>>>>>>>>>>>>>>;
 
 // Some helper functions to generate and get the images from the generator
-matrix<unsigned char> generate_image(generator_type& net, const noise_t& noise)
+matrix<rgb_pixel> generate_image(generator_type& net, const noise_t& noise)
 {
-    const matrix<float> output = net(noise);
-    matrix<unsigned char> image;
+    const auto output = net(noise);
+    matrix<rgb_pixel> image;
     assign_image(image, 255 * output);
     return image;
 }
 
-std::vector<matrix<unsigned char>> get_generated_images(const tensor& out)
+std::vector<matrix<rgb_pixel>> get_generated_images(const tensor& out)
 {
-    std::vector<matrix<unsigned char>> images;
-    for (size_t n = 0; n < out.num_samples(); ++n)
+    std::vector<matrix<rgb_pixel>> images;
+    for (long long n = 0; n < out.num_samples(); ++n)
     {
-        matrix<float> output = image_plane(out, n);
-        matrix<unsigned char> image;
-        assign_image(image, 255 * output);
+        matrix<rgb_pixel> image(out.nr(), out.nc());
+        for (size_t k = 0; k < 3; ++k) {
+            matrix<float> output = image_plane(out, n, k);
+            for (long y = 0; y < output.nr(); ++y) {
+                for (long x = 0; x < output.nc(); ++x) {
+                    const auto value = 255 * std::max(0.f, std::min(1.f, output(y, x)));
+                    switch (k) {
+                    case 0: image(y, x).red   = value; break;
+                    case 1: image(y, x).green = value; break;
+                    case 2: image(y, x).blue  = value; break;
+                    default: throw std::runtime_error("Unexpected k: " + std::to_string(k));
+                    }
+                }
+            }
+        }
         images.push_back(std::move(image));
     }
     return images;
@@ -124,24 +140,48 @@ std::vector<matrix<unsigned char>> get_generated_images(const tensor& out)
 
 int main(int argc, char** argv) try
 {
-    // This example is going to run on the MNIST dataset.
     if (argc != 2)
     {
-        cout << "This example needs the MNIST dataset to run!" << endl;
-        cout << "You can get MNIST from http://yann.lecun.com/exdb/mnist/" << endl;
-        cout << "Download the 4 files that comprise the dataset, decompress them, and" << endl;
-        cout << "put them in a folder.  Then give that folder as input to this program." << endl;
+        cout << "This example needs image data to run!" << endl;
+        cout << "Give a folder containing images as input to this program." << endl;
         return EXIT_FAILURE;
     }
 
-    // MNIST is broken into two parts, a training set of 60000 images and a test set of 10000
-    // images.  Each image is labeled so that we know what hand written digit is depicted.
-    // These next statements load the dataset into memory.
-    std::vector<matrix<unsigned char>> training_images;
-    std::vector<unsigned long>         training_labels;
-    std::vector<matrix<unsigned char>> testing_images;
-    std::vector<unsigned long>         testing_labels;
-    load_mnist_dataset(argv[1], training_images, training_labels, testing_images, testing_labels);
+    //const auto match = dlib::match_endings(".jpg .jpeg .png");
+
+    const auto match = [](const dlib::file& file) {
+        return file.name().find("zoom1") != std::string::npos && file.name().find("jpg") != std::string::npos;
+    };
+
+    const auto files = dlib::get_files_in_directory_tree(argv[1], match);
+
+    // Start a bunch of threads that read images from disk.
+    // It's important to be sure to feed the GPU fast enough to keep it busy.
+    // Using multiple thread for this kind of data preparation helps us do that.
+    // Each thread puts the images into the data queue.
+    dlib::pipe<matrix<rgb_pixel>> training_images(200);
+    auto keep_loading_images = [&training_images, &files](time_t seed)
+    {
+        dlib::rand rnd(time(0) + seed);
+        while (training_images.is_enabled())
+        {
+            const auto filename = files[rnd.get_random_64bit_number() % files.size()];
+            matrix<rgb_pixel> img;
+            load_image(img, filename);
+            if (img.nr() == 512 && img.nc() == 512) {
+                training_images.enqueue(img);
+            }
+        }
+    };
+
+    std::deque<std::thread> data_loaders;
+    for (int i = 0; i < 4; ++i) {
+        data_loaders.emplace_back(
+            [keep_loading_images, i]() {
+                keep_loading_images(i);
+            }
+        );
+    }
 
     // Fix the random generator seeds for network initialization and noise
     srand(1234);
@@ -176,7 +216,7 @@ int main(int argc, char** argv) try
         deserialize("dcgan_sync") >> generator >> discriminator >> iteration;
     }
 
-    const size_t minibatch_size = 64;
+    const size_t minibatch_size = 32;
     const std::vector<float> real_labels(minibatch_size, 1);
     const std::vector<float> fake_labels(minibatch_size, -1);
     dlib::image_window win;
@@ -184,11 +224,12 @@ int main(int argc, char** argv) try
     while (iteration < 50000)
     {
         // Train the discriminator with real images
-        std::vector<matrix<unsigned char>> real_samples;
+        std::vector<matrix<rgb_pixel>> real_samples;
         while (real_samples.size() < minibatch_size)
         {
-            auto idx = rnd.get_random_32bit_number() % training_images.size();
-            real_samples.push_back(training_images[idx]);
+            dlib::matrix<dlib::rgb_pixel> img;
+            training_images.dequeue(img);
+            real_samples.push_back(std::move(img));
         }
         // The following lines are equivalent to calling train_one_step(real_samples, real_labels)
         discriminator.to_tensor(real_samples.begin(), real_samples.end(), real_samples_tensor);
@@ -232,8 +273,8 @@ int main(int argc, char** argv) try
         generator.update_parameters(g_solvers, learning_rate);
 
         // At some point, we should see that the generated images start looking like samples from
-        // the MNIST dataset
-        if (++iteration % 1000 == 0)
+        // the training set
+        if (++iteration % 100 == 0)
         {
             serialize("dcgan_sync") << generator << discriminator << iteration;
             std::cout <<
